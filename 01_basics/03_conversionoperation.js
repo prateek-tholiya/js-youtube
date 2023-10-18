026c257458c03a3ddac1281db03bd2c72f734548
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2023-10-18 21:20:32 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+//let age = 33
+//let age = "33"
+//let age = "33abc" //
+//let age = null
+// let age = undefined  
+// console.log(typeof age) // console.log(typeof(age))  // or we can write in this way 
+
+
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber)  
+// console.log(valueInNumber) 
+
+// 33 => it will be easily converted to 33 
+// but as soon as we write 33 => "33abc" it will not be converted and will show NaN (not an number )in output
+// true => will be conveterted in 1 and false => in 0   
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+console.log(isLoggedIn)
+
+// 1=> output is true 0=> output is false 
+// "" => output is false 
+// "prateek" output is true 
