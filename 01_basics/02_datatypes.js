d442a1043479aab0a6ea597f18d43d50a4e46cbf
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2023-10-18 20:20:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" ; // treat all js code as a newer version 
+
+// alert (3 + 3) // we are using node js not browser to it will show error 
+
+console.log(3 + 3)
+// code readability should be high and important practice to be followed
+console.log("prateek")
+// types of data types 
+// 1. premitive type data type 
+
+let name = "prateek"   // string => ""
+let age = 23           // number => it has a limit of 2 power 53 
+                       // bigint => is used for bigger number 
+let isLoggedIn = true // boolean => true/false
+let state ;           // undefined  type 
+let city = null       // null => stand alone value 
+                      // symbol => used for unique ness
+
+//object 
+
+console.log(typeof age) // output => number 
+console.log(typeof null); // output =>  object
+console.log(typeof undefined); // output => undefined 
+                       
