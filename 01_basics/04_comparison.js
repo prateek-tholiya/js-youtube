3561ea3359033450edbc7a28e729f341cdb671da
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2023-10-19 20:54:05 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// basic conversion 
+
+// console.log(2>1)
+// console.log(2 >=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=0);
+
+// these have same datatypes so it will show direct answer as expected 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// here string will be converted into number so
+// output => true , true 
+
+// conversion which should be avoided
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(undefined == 0);
+console.log(undefined > 0 );
+console.log(undefined >= 0 );
