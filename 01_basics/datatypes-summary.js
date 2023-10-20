c5b1014fc7296f058505adc572c70e3126d5e0f9
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2023-10-20 09:45:41 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// primitive type 
+  // call by value 
+
+//  7 types - string , Number , Boolean, undefined , null ,  BigInt , symbol
+
+const number = 3424234    // number 
+ 
+const decimalNumber = 100.45 // number 
+const nameString = "prateek" // string 
+
+const isBool = true   // boolean 
+let thisNumber = null  // null 
+
+let city ;  // undefined 
+
+// reference type ( non-primitive type )
+
+// types - array , objects , function 
+
+// arrray 
+const heroes = ["shaktiman" , "nagraj" , "doga"]
+
+//objects - are written in curly braces {}
+
+let myObj = {
+    name:"prateek"
+    age: 23
+}
+
+//function - function is written like this 
+
+function(){
+    console.log("hello world ")
+}
