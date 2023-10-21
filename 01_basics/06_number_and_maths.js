a05c7bc1bca1330ab94ae7966521136f8f8fe9c7
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2023-10-21 13:56:59 +0530

    number&maths

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,8 @@
+// number 
+
+const score = 400 
+
+console.log(score) // output =>400
+// this will automatically detect type of datatype 
+const balance = new Number(100)  //but still if you want confirmly number than you can write like this 
+console.log(balance)
