945bfe7d261e26ef9ddbe6dcaf4b3233bd3e9a0d
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2023-10-21 19:07:01 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,28 @@
+//array 
+
+const arry = [3, 5, 6, 3 , 8] //in arrray  brackets are used [] to form arrya and inside array we can put number , string 
+                               //any thing together and inside array we call it element 
+      
+const myHeors = ["batman " , "superman"]
+ 
+const arry2 = new Array(1,2,3,4,5) //here no need to put brackets it automatically creates brackets 
+// console.log(myHeors[0]);  // by using index we access array 
+
+
+//array methods 
+
+// arry.push(6)  // this method adds element in array 
+// arry.push(8) 
+// arry.pop()       // in pop() we dont need to put any element it just delete last element from array 
+
+// arry.unshift(9)   // this add element at the first index of array but we use this very rare as this changes whole numbering of array 
+//                  // and every element need to be shifted 
+       
+// arry.shift()    // this removes element  at start index     
+
+// console.log(arry.includes(5)); // this checks if element in array exist or not 
+
+// console.log(arry.indexOf(5))  // it will give index if element is present if not present it will give -1 
+
+// splice and slice must read 
+//https://www.geeksforgeeks.org/what-is-the-difference-between-array-slice-and-array-splice-in-javascript/
