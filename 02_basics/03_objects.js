49cef5634a99f736b39398f1c0e65c289d80d0ed
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2023-10-22 19:18:42 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,10 @@
+// single ton  - when you declare object as literal than no singleton is formed 
+// but if you make it from a constructor  it will be alaways be a singleton 
+
+// object.create //  this is constructor method to create object 
+
+
+// object leterals 
+
+const jsUser = {}
+
