2102a71ffb9fffd117b598952feee307846fdcf1
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2023-11-01 23:55:20 +0000

    forof loop

---
+++ 05_iterations/forof_loop.js
@@ -0,0 +1,23 @@
+// for of loops 
+// this is syntax for of loops 
+// for (const iterator of object) {
+    
+// }
+// for (const num of arr){
+//     console.log(num);
+// }
+
+// const greetings ="hello world"
+// for (const greet of greetings) {
+//     console.log(`each char is ${greet}`);
+// }
+
+
+//maps 
+const map = new Map()
+map.set("IN" , "India")
+map.set("USA" ,"United States Of America")
+map.set("Fr" , "France")
+map.set("IN" ,"India")
+
+console.log(map);
